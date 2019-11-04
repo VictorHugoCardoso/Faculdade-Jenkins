86ceed5e82f5688e9617c14e4af83d9075986446
note <victor.mendes056@gmail.com>
2019-11-04 08:38:58 -0300

    allow host

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6#bq8r2npb20!u4c91lku5_jjiozz%tf11pc3q!4yaqea_z=lg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
